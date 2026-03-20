655cf3fdfbb05a2b470315223418d6ed7595b08a
makkalnalvazhvusangam <mnsambalnagar@gmail.com>
2026-03-20 23:28:26 +0530

    fix esm + supabase

--- supabase.js
+++ supabase.js
@@ -1,9 +1,11 @@
-const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
+import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
-module.exports = supabase;
+export default supabase;
+
+
+
