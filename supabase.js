af6b5965d1e4a485809d237b3af3e4c534765886
makkalnalvazhvusangam <mnsambalnagar@gmail.com>
2026-03-21 01:47:05 +0530

    fix supabase env

--- supabase.js
+++ supabase.js
@@ -1,11 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
+require('dotenv').config();
+
+const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
 );
 
-export default supabase;
-
-
-
+module.exports = supabase;
